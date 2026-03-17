2f10e419a250803692c058437f6c72ff0a2e6ac2
joatmon11 <joat.mon.11@gmail.com>
2026-03-16 18:38:13 -0700

    Finshed Landing and Highilghts.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import './App.css';
 import Nav from './components/Nav';
+import Landing from './components/Landing';
+import Highlights from './components/Highlights';
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
