324f7396c36c8762f543ea0f3c57108476b85c91
joatmon11 <joat.mon.11@gmail.com>
2026-03-18 16:11:48 -0700

    Added through Books Routing.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Books from './pages/Books';
 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home}/>
+        <Route path="/books" component={Books} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
