e37420af59b061bcd2ef680f94db604d91e4673e
joatmon11 <joat.mon.11@gmail.com>
2026-03-20 19:20:27 -0700

    Added through Recommended Books, everything working.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from './components/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Books from './pages/Books';
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
 
 
 function App() {
@@ -12,7 +14,8 @@ function App() {
       <div className="App">
         <Nav />
         <Route path="/" exact component={Home}/>
-        <Route path="/books" component={Books} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/:id" exact render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router>
